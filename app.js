698614f74f924d41576b4175fe405028fc354864
razzcoder <rajatsbhadauria@gmail.com>
2016-10-10 03:01:10 +0530

    Changed Something

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app=angular.module("myApp",['mainController','mainDirective','ngAnimate', 'ui.router','CommonData','ngMaterial','ngMessages','SharedServiceFactoryModule','mainService','hljs'])
+var app=angular.module("myApp",['mainController','mainDirective','ngAnimate', 'ui.router','CommonData','ngMaterial','ngMessages','SharedServiceFactoryModule','mainService'])
 
 
 .config(function($stateProvider, $urlRouterProvider,$httpProvider,hljsServiceProvider)
